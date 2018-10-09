af717d67290567044d8f623993a0a23c01c3dcd5
Florent Uguet <florent.uguet@gmail.com>
2018-10-09 15:05:16 +0200

    Added more constructors

--- lib/api.dart
+++ lib/api.dart
@@ -218,6 +218,49 @@ class Part {
   //Specs
   Map<String, SpecValue> specs;
   List<String> category_uids;
+
+  Part.fromJson(dynamic oJson){
+    this.uid = oJson['uid'];
+    this.mpn = oJson['mpn'];
+    this.manufacturer = Manufacturer.fromJson(oJson['manufacturer']);
+    this.brand = Brand.fromJson(oJson['brand']);
+    this.octopart_url = oJson['octopart_url'];
+    this.external_links = oJson['external_links'];
+
+    //Pricing
+    this.offers.clear();
+    for(var i=0;i<oJson['offers'].length;i++) this.offers.add(PartOffer.fromJson(oJson['offers'][i]));
+  
+    this.broker_listings = BrokerListing.fromJson(oJson['broker_listings']);
+
+    //Description
+    this.short_description = oJson['short_description'];
+
+    this.descriptions.clear();
+    for(var i=0;i<oJson['descriptions'].length;i++) this.descriptions.add(Description.fromJson(oJson['descriptions'][i]));
+
+    this.imagesets.clear();
+    for(var i=0;i<oJson['imagesets'].length;i++) this.imagesets.add(ImageSet.fromJson(oJson['imagesets'][i]));
+    
+    //Documents
+    this.datasheets.clear();
+    for(var i=0;i<oJson['datasheets'].length;i++) this.datasheets.add(Datasheet.fromJson(oJson['datasheets'][i]));
+
+    this.compliance_documents.clear();
+    for(var i=0;i<oJson['compliance_documents'].length;i++) this.compliance_documents.add(ComplianceDocument.fromJson(oJson['compliance_documents'][i]));
+
+    this.reference_designs.clear();
+    for(var i=0;i<oJson['reference_designs'].length;i++) this.reference_designs.add(ReferenceDesign.fromJson(oJson['reference_designs'][i]));
+
+    this.cad_models.clear();
+    for(var i=0;i<oJson['cad_models'].length;i++) this.cad_models.add(CADModel.fromJson(oJson['cad_models'][i]));
+    
+    //Specs
+    //TODO : Map<String, SpecValue> specs;
+
+    this.category_uids.clear();
+    for(var i=0;i<oJson['category_uids'].length;i++) this.category_uids.add(oJson['category_uids'][i]);
+  }
 }
 
 class PartOffer {
@@ -245,7 +288,7 @@ class PartOffer {
     eligible_region = oJson['eligible_region'];
     product_url = oJson['product_url'];
     octopart_rfq_url = oJson['octopart_rfq_url'];
-    Map<String, Map<int, double>> prices;
+    //TODO : Map<String, Map<int, double>> prices;
     in_stock_quantity = oJson['in_stock_quantity'];
     on_order_quantity = oJson['on_order_quantity'];
     on_order_eta = DateTime.parse(oJson['on_order_eta']);
@@ -266,12 +309,26 @@ class SpecValue {
   double max_value;
   SpecMetadata metadata;
   Attribution attribution;
+
+  SpecValue.fromJson(dynamic oJson){
+    value = oJson['value'];
+    min_value = oJson['min_value'];
+    max_value = oJson['max_value'];
+    metadata = SpecMetadata.fromJson(oJson['metadata']);
+    attribution = Attribution.fromJson(oJson['attribution']);
+  }
 }
 
 class ReferenceDesign extends Asset {
   String title;
   String description;
   Attribution attribution;
+
+  ReferenceDesign.fromJson(dynamic oJson) : super.fromJson(oJson){
+    title = oJson['title'];
+    description = oJson['description'];
+    attribution = Attribution.fromJson(oJson['attribution']);
+  }
 }
 
 class Seller {
@@ -280,11 +337,24 @@ class Seller {
   String homepage_url;
   String display_flag;
   bool has_ecommerce;
+
+  Seller.fromJson(dynamic oJson) {
+    uid = oJson['uid'];
+    name = oJson['name'];
+    homepage_url = oJson['homepage_url'];
+    display_flag = oJson['display_flag'];
+    has_ecommerce = oJson['has_ecommerce'];
+  }
 }
 
 class Source {
   String uid;
   String name;
+
+  Source.fromJson(dynamic oJson) {
+    uid = oJson['uid'];
+    name = oJson['name'];
+  }
 }
 
 class SpecMetadata{
@@ -292,11 +362,23 @@ class SpecMetadata{
   String name;
   String datatype;
   UnitOfMeasurement unit;
+
+  SpecMetadata.fromJson(dynamic oJson) {
+    key = oJson['key'];
+    name = oJson['name'];
+    datatype = oJson['datatype'];
+    unit = UnitOfMeasurement.fromJson(oJson['unit']);
+  }
 }
 
 class UnitOfMeasurement {
   String name;
   String symbol;
+
+  UnitOfMeasurement.fromJson(dynamic oJson) {
+    name = oJson['name'];
+    symbol = oJson['symbol'];
+  }
 }
 
 class PartsMatchRequest {
